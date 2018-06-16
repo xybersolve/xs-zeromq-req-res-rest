31447edc965c1eca713fd14d579f77de04793d69
xybersolve <xybersolve@gmail.com>
2018-06-15 18:19:13 -0700

    remove docker-machine from Makefile, for Jenkins docker session

--- env.mk
+++ env.mk
@@ -4,8 +4,9 @@
 ORG := xybersolve
 PROJECT := xs-zmq-req-res-rest
 
-HOST_NAME := $(shell docker-machine active)
-HOST_IP := $(shell docker-machine ip $(HOST_NAME))
+# won't work in Jenkins docker - only used for 'open'
+#HOST_NAME := $(shell docker-machine active)
+#HOST_IP := $(shell docker-machine ip $(HOST_NAME))
 HTTP_PORT := 8080
 
 GIT_SHORT := $(shell git log -1 --pretty=%h)
